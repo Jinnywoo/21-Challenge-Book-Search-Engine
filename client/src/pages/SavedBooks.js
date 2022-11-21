48b90bf6152289bd7c477523b9442647b6b2e51a
Andrew <105199424+Jinnywoo@users.noreply.github.com>
2022-11-21 15:31:33 -0800

    asved and esarched book working on auth token

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  // const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery( GET_ME );
+
+  const [removeBook] = useMutation( REMOVE_BOOK );
+  
+  const userData = data?.me || {};
+  console.log(userData);
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
